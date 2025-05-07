ace840bef4255d883e9f23e516179fa6f9dce596
Konrad <115174801+KonradSzkolka@users.noreply.github.com>
2025-05-07 21:01:30 +0200

    1

--- Zadania_Domowe_Sem_IV/src/Main.java
+++ Zadania_Domowe_Sem_IV/src/Main.java
@@ -5,10 +5,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Wzory Matematyczne");
-        System.out.print("Podaj długość boku kwadratu aby wyliczyć objętość: ");
+        System.out.print("Podaj długość boku kwadratu aby wyliczyć pole: ");
         Scanner sc = new Scanner(System.in); //zatrzymuje działanie programu i czeka na interakcję użytkownika
-        int sideLengthsc = sc.nextInt(); //przypisujemy wartość "zeskanowaną do zmiennej"
-        System.out.println("Podałes wartość: " + sideLengthsc + " Obwód kwadratu = " + (4 * sideLengthsc));
+        int sideLength = sc.nextInt(); //przypisujemy wartość "zeskanowaną do zmiennej"
+        System.out.println("Podałes wartość: " + sideLength + " pole kwadratu = " + (sideLength * sideLength));
+        //scaner podajemy raz
+        System.out.print("Podaj długość 1 boku prostokąta aby wyliczyć pole: ");
+        int sideLengthsProstokont = sc.nextInt();
+        System.out.print("Podaj długość 2 boku prostokąta aby wyliczyć pole: ");
+        int sideLengthsProstokont2 = sc.nextInt();
+        System.out.print("podaleś 1 bok " + sideLengthsProstokont + " podałeś 2 bok " + sideLengthsProstokont2 + "pole prostokonta = " + (sideLengthsProstokont2 * sideLengthsProstokont));
 
     }
 }
